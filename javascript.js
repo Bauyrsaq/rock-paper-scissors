5f7804cddba215952e44818254f221814523fa5b
Bauyrsaq <ali_talap@mail.ru>
2023-02-03 09:28:55 -0500

    Finish game

--- javascript.js
+++ javascript.js
@@ -9,70 +9,112 @@ function getComputerChoice() {
     }
 }
 
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'Rock') {
-            return "Draw! Rock and Rock";
-        } else if (computerSelection === 'Scissors') {
-            return "You Win! Rock beats Scissors";
-        } else {
-            return "You Lose! Paper beats Rock";
-        }
-    }
-
-    if (playerSelection === 'paper') {
-        if (computerSelection === 'Paper') {
-            return "Draw! Paper and Paper";
-        } else if (computerSelection === 'Rock') {
-            return "You Win! Paper beats ROck";
-        } else {
-            return "You Lose! Scissors beats Paper";
-        }
-    }
-
-    if (playerSelection === 'scissors') {
-        if (computerSelection === 'Scissors') {
-            return "Draw! Scissors and Scissors";
-        } else if (computerSelection === 'Paper') {
-            return "You Win! Scissors beats Paper";
-        } else {
-            return "You Lose! Rock beats Scissors";
-        }
+    if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+        playerSelection === 'paper' && computerSelection === 'rock' ||
+        playerSelection === 'scissors' && computerSelection === 'paper') {
+            playerScore++;
+            displayResult(`${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`);
+    } else if (playerSelection === computerSelection) {
+        displayResult('Tie round');
+    } else {
+        computerScore++;
+        displayResult(`${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`);
     }
 }
 
+function displayResult(result) {
+    scoreTitle.textContent = result;
+    player.textContent = playerScore;
+    computer.textContent = computerScore;
+}
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Choice Rock, Paper or Scissors", "Rock");
-        const computerSelection = getComputerChoice();
-        const answerTheGame = playRound(playerSelection, computerSelection);
-        
-        if (answerTheGame.search("Win") !== -1) {
-            playerScore += 1;
-        } else if (answerTheGame.search("Lose") !== -1) {
-            computerScore += 1;
-        }
+function rplContent() {
+    main.classList.add('disappear');
+    endAlert.classList.remove('disappear');
 
-        console.log(answerTheGame);
-    }
+    retryBtn.addEventListener('click', () => {
+        main.classList.remove('disappear');
+        endAlert.classList.add('disappear');
+        resetGame();
+    });
+}
 
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    player.textContent = playerScore;
+    computer.textContent = computerScore;
+    playerWeapon.textContent = '?';
+    computerWeapon.textContent = '?';
+}
+
+function gameEnd() {
+    rplContent();
+
+    endDesc.innerHTML = '';
     if (playerScore > computerScore) {
         console.log("You are winner!");
-    } else if (playerScore < computerScore) {
-        console.log("You are loser!");
+        endDesc.textContent = "You are winner!";
     } else {
-        console.log("Draw!");
+        console.log("You are loser!");
+        endDesc.textContent = "You are loser!";
     }
 
-    console.log(`Player score: ${playerScore}`);
-    console.log(`Computer score: ${computerScore}`);
+}
+
+function createWeapon(playerSelection, computerSelection) {
+    const playerWeaponRound = document.createElement('img');
+    const computerWeaponRound = document.createElement('img');
+
+    playerWeaponRound.src = `images/${playerSelection.toLowerCase()}.png`;
+    computerWeaponRound.src = `images/${computerSelection.toLowerCase()}.png`;
+
+    playerWeaponRound.alt = playerSelection;
+    computerWeaponRound.alt = computerSelection;
+
+    playerWeapon.textContent = '';
+    computerWeapon.textContent = '';
+
+    playerWeapon.appendChild(playerWeaponRound);
+    computerWeapon.appendChild(computerWeaponRound);
 }
 
 
-game();
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('.button');
+const scoreTitle = document.querySelector('.score-title');
+scoreTitle.innerHTML = '';
+const player = document.querySelector('.player-score');
+const computer = document.querySelector('.computer-score');
+const playerWeapon = document.querySelector('.player-weapon');
+const computerWeapon = document.querySelector('.computer-weapon');
+const main = document.querySelector('#main-content');
+const endAlert = document.querySelector('#end-alert');
+const endDesc = document.querySelector('#end-desc');
+const retryBtn = document.querySelector('#retry-btn');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const playerImg = button.querySelector('img');
+        
+        const playerSelection = playerImg.alt;
+        const computerSelection = getComputerChoice();
+
+        createWeapon(playerSelection, computerSelection);
+
+        playRound(playerSelection, computerSelection);
+        if (playerScore === 5 || computerScore === 5) {
+            gameEnd();
+        }
+    })
+})
